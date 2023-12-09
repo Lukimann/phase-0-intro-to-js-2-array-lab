165d9d240808fcabd5fb181ca211392dc9a887e9
Luka Wanyama <lukewanyama5@gmail.com>
2023-12-09 13:48:27 +0300

    To be continued...

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"] 
+
+function destructivelyAppendCat(Yoyo) {
+    cats.push(Yoyo);
+}
+
+function destructivelyPrependCat(vader) {
+    cats.unshift(vader)
+}
+
+function destructivelyRemoveLastCat(Fluffy) {
+    cats.pop(Fluffy)
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo)
+}
+
+//I COULDN'T QUITE MOVE FROM THIS ASSIGNMENT. I'M EXPERIENCING A BLOCKER THAT DOESN'T SEEM TO GO AWAY.
+
+function prependCat(Truble) {
+    let CatSeys = [Truble, ...cats] 
+}
+
+
+
+//
