1a8a3d252c98243b871ec3ed165b93c63e006144
Luka Wanyama <lukewanyama5@gmail.com>
2023-12-15 18:28:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -18,11 +18,30 @@ function destructivelyRemoveFirstCat(Milo) {
     cats.shift(Milo)
 }
 
+function appendCat() {
+    return cats.concat('Broom')
+}
+
+const prependCat = () =>  {
+    return ['Arnold', ...cats]
+}
+
+const removeLastCat = () => {
+    return cats.slice(0, -1)
+}
+
+const removeFirstCat = () => {
+    return cats.slice(1)
+}
+
+//let appendCat = cats.concat('Broom');
+
+
 //I COULDN'T QUITE MOVE FROM THIS ASSIGNMENT. I'M EXPERIENCING A BLOCKER THAT DOESN'T SEEM TO GO AWAY.
 
-function prependCat(Truble) {
-    let CatSeys = [Truble, ...cats] 
-}
+//function prependCat(Truble) {
+  //  let CatSeys = [Truble, ...cats] 
+//}
 
 
 
